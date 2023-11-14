a85cda421115c09045fe7a2304c7a045c875641a
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-14 20:28:02 +0100

    V 0.1.1.

--- SQL/MsSQL_euprojectapp.sql
+++ SQL/MsSQL_euprojectapp.sql
@@ -8,17 +8,17 @@ use EUprojectsApp;
 
 create table EUprojects (
 id int not null primary key identity (1,1), 
-uniqueIDnumber varchar(50) not null,
-name varchar(50) not null,
-dateStart datetime, 
-dateEnd datetime,
-activities int,  
+uniqueIDnumber varchar(50) not null, -- identifikacijska sifra projekta npr. K.K. 1012
+name varchar(50) not null, -- puni naziv projekta
+dateStart datetime,  
+dateEnd datetime, 
+isFinished bit 
 );
 
 create table teams(
 team int not null, 
 teamName varchar(50) not null, -- po projektu npr. Tim - Urbana aglomeracija Osijek ili RCK VirtuOS
-member int not null, 
+member int not null,  
 EUproject int not null,
 activity int not null
 );
@@ -29,13 +29,15 @@ name varchar(50),
 lastName varchar(50),
 userName varchar(50),
 password varchar(50),
-isTeamLeader bit
+activity int,
+isTeamLeader bit -- if true - pravo dodijeljivanja aktivnosti clanovima, promjene rokova veifikacije 
+-- dokaznica
 );
 
 create table activities(
 id int not null primary key identity(1,1),
 name varchar(50),
-description varchar(500),
+description varchar(500),  
 dateStart datetime, 
 dateFinish datetime,
 isFinished bit
@@ -43,11 +45,8 @@ isFinished bit
 
 create table proofOfDelivery(
 id int not null primary key identity(1,1), 
-organisation int,
-description int,
-contractName varchar(50), 
-EuProjectName varchar(50),
-
+documentName varchar(50) not null -- dokument dokaznice npr. izvjesce o provedenoj 
+--aktivnosti 4.1. izrada cost benefit analize 
 );
 
 create table connector(
@@ -58,10 +57,11 @@ EUproject int not null
 create table folders(
 id int not null primary key identity(1,1), 
 location varchar(50) not null,
-contractActivityName varchar(50) not null -- ovo je ujedno i folder name. Ako je u ugovoru 
+contractActivityName varchar(50) not null, -- ovo je ujedno i folder name. Ako je u ugovoru 
 -- naziv aktivnosti 1.2. izrada komunikacijske strategije ili 3.1. zakup medijskog prostora 
 -- nacionalnih radio postaja zbog kvalitete organizacije korisnik ne treba imati mogucnost
 -- slobodnog odabira naziva 
+proofOfDelivery int
 );
 
 create table activitiesConnector(
@@ -72,4 +72,7 @@ activity int not null
 alter table connector add foreign key (folder) references folders(id); 
 alter table connector add foreign key (EUproject) references EUprojects(id); 
 alter table teams add foreign key (team) references EUprojects(id);
-alter table teams add foreign key (member) references members(id); 
+alter table teams add foreign key (member) references members(id);
+alter table activitiesConnector add foreign key (EUproject) references EUprojects(id);
+alter table activitiesConnector add foreign key (activity) references activities(id); 
+alter table folders add foreign key (id) references proofOfDelivery(id); 
