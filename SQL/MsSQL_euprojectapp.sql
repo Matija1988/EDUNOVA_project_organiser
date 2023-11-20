2c052546a9d5306286e08868f926790586630726
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-20 20:28:39 +0100

    Update MsSQL_euprojectapp.sql

--- SQL/MsSQL_euprojectapp.sql
+++ SQL/MsSQL_euprojectapp.sql
@@ -31,13 +31,13 @@ isTeamLeader bit -- if true - pravo dodjeljivanja aktivnosti clanovima, promjene
 
 create table activities(
 id int not null primary key identity(1,1),
-name varchar(50) not null,
+name varchar(100) not null,
 description varchar(500),  
 dateStart datetime not null, -- planirani pocetak aktivnosti
 dateFinish datetime not null, -- planirani kraj aktivnosti
 folderID int,
 isFinished bit, -- team leader ima ovlastenje zatvoriti pojedinu aktivnost
-dateFinished datetime, -- kraj aktivnosti
+dateAccepted datetime, -- kraj aktivnosti
 projectID int not null -- jedna aktivnost pripada jednom projektu  
 );
 
@@ -53,7 +53,7 @@ dateCreated datetime
 create table folders( -- uzima dokaznice sa više lokacija i objedinjuje ih 
 id int not null primary key identity(1,1), 
 location varchar(100) not null,
-contractActivityName varchar(50) not null, -- ovo je ujedno i folder name. 
+contractActivityName varchar(100) not null, -- ovo je ujedno i folder name. 
 proofOfDelivery int 
 );
 
@@ -69,23 +69,42 @@ alter table folders add foreign key (id) references proofOfDelivery(id);
 
 insert into projects (uniqueIDnumber, name, dateStart, dateEnd, isFinished) 
 values ('JP21-21KD', 'Poboljšanje energetske učinkovitosti zgrade Zamišljena Adresa 2', '2023-5-21 09:00:00', 
-'2024-11-21 12:00:00', 0);
+'2024-11-21 12:00:00', 0),('UZ-54-2-I','Uspostava regionalnog centra kompetentnosti Srednja strukovna
+škola Bubimir','2021-1-1 09:00:00','2023-12-29',0), ('A4-K-7-762','Zelene staze Dunava i Drave',
+'2019-4-12 8:00:00','2022-5-1 17:00:00',1);
 
 insert into proofOfDelivery (documentName, location, dateCreated)
-values ('Analiza troškova i koristi','C:\user\documents\','2023-12-15 12:00:00');
+values ('Analiza troškova i koristi','C:\user\documents\','2023-12-15 12:00:00'),
+('RCKSSB marketing plan Otvoreni radio','C:\user\documents','2021-10-10 11:00:00'), 
+('Komunikacijska strategija','F:\A4-K-7-762\1.3 Izrada komunikacijske strategije\','2019-10-22'),
+('Zapisnik sa koordinacijskog sastanka', 'D:\razno\', '2021-1-15 11:00:00');
 
 insert into folders (location, contractActivityName, proofOfDelivery)
-values ('d:\JP21-21KD\1.1. Izrada analize troškova i korsti','Analiza troškova i koristi',1);
+values ('d:\JP21-21KD\1.1. Izrada analize troškova i korsti','Analiza troškova i koristi',1),
+('d:\UZ-54-2-I\1.3 Izrada komunikacijske strategije\', 'Izrada komunikacijske strategije',2),
+('d:\A4-K-7-762\2.1. Zakup medijskog prostora na nacionalnim radio postajama','Potvrda o emitiranju
+Otvoreni radio',3), ('d:\UZ-54-2-I\4.1. Koordinacijski sastanci\4.1.1.\','Dokaznica o održanom mjesećnom 
+koordinacijskom sastanku 2021_1_15',4);
 
-insert activities (name, description, dateStart, dateFinish, folderID, isFinished, dateFinished, projectID)
+insert activities (name, description, dateStart, dateFinish, folderID, isFinished, dateAccepted, projectID)
 values ('1.1.Izrada analize troškova i koristi','Energetska obnova zgrade ... mora uključivati
 izolaciju vanjskog zida, dizalicu topline, ugradnju PVC stolarije, fotonaponske ćelije 12 kw/h ...
-','2023-6-1 09:00:00','2023-8-1 09:00:00',1, 1,'2023-7-29 12:00:00', 1);
+','2023-6-1 09:00:00','2023-8-1 09:00:00',1, 1,'2023-7-29 12:00:00', 1),
+('2.1. Zakup medijskog prostora na nacionalnim radio postajama','Izrada radio jingla 30 sekundi, zakup
+nacionalne radio postaje za 50 emitiranja','2021-02-15 11:00:00','2021-03-28 17:00:00',3,0,
+'2021-04-01 09:00:00',2),
+('1.3 Izrada komunikacijske strategije','Komunikacijska strategija promocije eko-edukativne staze na 
+potezu PP Drava-Mura mora uključivati zakup 2x lokalnih, 1x regionalnih i 1x nacionalnih radio postaja, 
+2x nacionalne dnevne novine ....','2019-5-1 09:00','2019-6-1 11:00:00',2,1,'2019-6-3 12:00:00',3),
+('4.1. Koordinacijski sastanci','Izvršitelj je dužan napraviti zapisnik, držati
+potpisne liste te osigurati fotografski dokaz mjesečnih koordinacijskih sastanaka','2021-1-15 09:00:00',
+'2021-1-16 11:00:00',4,1,'2021-1-16 13:00:00',2);
 
 insert into members (name, lastName, userName, password, isTeamLeader)
-values ('Chuck','Norris','N0rr1s','Sifra12345678889',1);
+values ('Chuck','Norris','N0rr1s','Sifra12345678889',1),('Marko','Marković','MM007','JamesBlond069',0),
+('Petar','Bočkaj','Guc2A','OsvetaKanižlićeve',0);
 
 insert into activitiesConnector(activityID, memberID) 
-values (1,1); 
+values (1,1),(2,1),(3,1),(1,2),(2,3),(4,3); 
 
 
