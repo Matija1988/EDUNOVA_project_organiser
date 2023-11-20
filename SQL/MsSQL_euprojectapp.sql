77dcfa3879d50101aec6efd7e73e7c7362c58f88
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-20 21:04:45 +0100

    Update MsSQL_euprojectapp.sql

--- SQL/MsSQL_euprojectapp.sql
+++ SQL/MsSQL_euprojectapp.sql
@@ -71,14 +71,17 @@ insert into projects (uniqueIDnumber, name, dateStart, dateEnd, isFinished)
 values ('JP21-21KD', 'Poboljšanje energetske učinkovitosti zgrade Zamišljena Adresa 2', '2023-5-21 09:00:00', 
 '2024-11-21 12:00:00', 0),('UZ-54-2-I','Uspostava regionalnog centra kompetentnosti Srednja strukovna
 škola Bubimir','2021-1-1 09:00:00','2023-12-29',0), ('A4-K-7-762','Zelene staze Dunava i Drave',
-'2019-4-12 8:00:00','2022-5-1 17:00:00',1);
+'2019-4-12 8:00:00','2022-5-1 17:00:00',1),
+('INTER-HU-CRO-213-D-91','INTEREG-Hungary-Croatia cluster sustainability du dah','2023-1-20 10:00:00',
+'2023-10-20',1);
 
 insert into proofOfDelivery (documentName, location, dateCreated)
 values ('Analiza troškova i koristi','C:\user\documents\','2023-12-15 12:00:00'),
 ('RCKSSB marketing plan Otvoreni radio','C:\user\documents','2021-10-10 11:00:00'), 
 ('Komunikacijska strategija','F:\A4-K-7-762\1.3 Izrada komunikacijske strategije\','2019-10-22'),
 ('Zapisnik sa koordinacijskog sastanka', 'D:\razno\', '2021-1-15 11:00:00'),
-('export.zip', 'G:\foto\', '2020-4-17 21:00:00');
+('export.zip', 'G:\foto\', '2020-4-17 21:00:00'),
+('Mađari predavanje','D:\','2023-5-5 10:00:00');
 
 insert into folders (location, contractActivityName, proofOfDelivery)
 values ('d:\JP21-21KD\1.1. Izrada analize troškova i korsti','Analiza troškova i koristi',1),
@@ -86,7 +89,8 @@ values ('d:\JP21-21KD\1.1. Izrada analize troškova i korsti','Analiza troškova
 ('d:\A4-K-7-762\2.1. Zakup medijskog prostora na nacionalnim radio postajama','Potvrda o emitiranju
 Otvoreni radio',3), ('d:\UZ-54-2-I\4.1. Koordinacijski sastanci\4.1.1.\','Dokaznica o održanom mjesećnom 
 koordinacijskom sastanku 2021_1_15',4), 
-('d:\A4-K-7-762\3.2. Redovno fotografiranje aktivnosti projekta\','Postavljanje edukativnih tabli.zip',5);
+('d:\A4-K-7-762\3.2. Redovno fotografiranje aktivnosti projekta\','Postavljanje edukativnih tabli.zip',5),
+('d:\INTER-REG HU-CRO','2.1. Educational workshop on agricultural sustainability',6);
 
 insert activities (name, description, dateStart, dateFinish, folderID, isFinished, dateAccepted, projectID)
 values ('1.1.Izrada analize troškova i koristi','Energetska obnova zgrade ... mora uključivati
@@ -103,13 +107,18 @@ potpisne liste te osigurati fotografski dokaz mjesečnih koordinacijskih sastana
 '2021-1-16 11:00:00',4,1,'2021-1-16 13:00:00',2),
 ('3.2. Redovno fotografiranje aktivnosti projekta','Na poziv Naručitelja Izvršitelj je dužan fotografirati
 ključne faze provedbe projekta, 10x dostvljanje min rezolucije 4000x3000 300dpi','2020-4-17 8:00:00',
-'2020-4-17 21:00:00',5, 1,'2020-4-18 12:00:00', 3);
+'2020-4-17 21:00:00',5, 1,'2020-4-18 12:00:00', 3),
+('2.1. Educational workshop on agricultural sustainability','Organizacija edukativne radionice 
+na temu održivosti u poljoprivredi - obavezan predavač iz Mađarske (kontaktirati partnere) i iz
+Hrvatske - osigurati audio tehniku i projektor - minimalno 30 sudionika - catering za 40 osoba - 
+promocija na lokalnom web protalu (banner na 10 000 impresija i pr članak)','2023-5-4 9:00:00',
+'2023-5-4 11:00:00',6,1,'2023-5-7 13:00:00',4);
 
 insert into members (name, lastName, userName, password, isTeamLeader)
 values ('Chuck','Norris','N0rr1s','Sifra12345678889',1),('Marko','Marković','MM007','JamesBlond069',0),
 ('Petar','Bočkaj','Guc2A','OsvetaKanižlićeve',0);
 
 insert into activitiesConnector(activityID, memberID) 
-values (1,1),(2,1),(3,1),(1,2),(2,3),(4,3), (5,1); 
+values (1,1),(2,1),(3,1),(1,2),(2,3),(4,3),(5,1), (6,2); 
 
 
