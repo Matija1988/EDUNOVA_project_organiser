2a28e01b00d9c9c937016b346f97114819836326
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-12-05 17:38:56 +0100

    brze izmjene

--- SQL/MsSQL_euprojectapp.sql
+++ SQL/MsSQL_euprojectapp.sql
@@ -11,7 +11,7 @@ use projectsApp;
 create table projects (
 id int not null primary key identity (1,1), 
 uniqueIDnumber varchar(50) not null, -- identifikacijska sifra projekta npr. K.K. 1012
-name varchar(100) not null, -- puni naziv projekta
+projectName varchar(100) not null, -- puni naziv projekta
 dateStart datetime,  
 dateEnd datetime, 
 isFinished bit,
@@ -21,7 +21,7 @@ isFinished bit,
 
 create table members(
 id int not null primary key identity(1,1),
-name varchar(50),
+firstName varchar(50),
 lastName varchar(50),
 userName varchar(50),
 password varchar(100),
@@ -31,7 +31,7 @@ isTeamLeader bit -- if true - pravo dodjeljivanja aktivnosti clanovima, promjene
 
 create table activities(
 id int not null primary key identity(1,1),
-name varchar(100) not null,
+activityName varchar(100) not null,
 description varchar(500),  
 dateStart datetime not null, -- planirani pocetak aktivnosti
 dateFinish datetime not null, -- planirani kraj aktivnosti
@@ -67,7 +67,7 @@ alter table activitiesConnector add foreign key (activityID) references activiti
 alter table activitiesConnector add foreign key (memberID) references members(id);
 alter table folders add foreign key (id) references proofOfDelivery(id); 
 
-insert into projects (uniqueIDnumber, name, dateStart, dateEnd, isFinished) 
+insert into projects (uniqueIDnumber, projectName, dateStart, dateEnd, isFinished) 
 values ('JP21-21KD', 'Poboljšanje energetske učinkovitosti zgrade Zamišljena Adresa 2', '2023-5-21 09:00:00', 
 '2024-11-21 12:00:00', 0),('UZ-54-2-I','Uspostava regionalnog centra kompetentnosti Srednja strukovna
 škola Bubimir','2021-1-1 09:00:00','2023-12-29',0), ('A4-K-7-762','Zelene staze Dunava i Drave',
@@ -102,7 +102,7 @@ values
 '7.12.Videozapis o projektnoj aktivnosti stručnog usavršavanja mentora kod poslodavaca.zip',7),
 ('d:\UZ-54-2-I\4.1. Koordinacijski sastanci\4.1.2.\','Dokaznica o održanom mjesečnom 
 koordinacijskom sastanku 2021_2_20',4); 
-insert activities (name, description, dateStart, dateFinish, folderID, isFinished, dateAccepted, projectID)
+insert activities (activityName, description, dateStart, dateFinish, folderID, isFinished, dateAccepted, projectID)
 values ('1.1.Izrada analize troškova i koristi','Energetska obnova zgrade ... mora uključivati
 izolaciju vanjskog zida, dizalicu topline, ugradnju PVC stolarije, fotonaponske ćelije 12 kw/h ...
 ','2023-6-1 09:00:00','2023-8-1 09:00:00',1, 1,'2023-7-29 12:00:00', 1),
@@ -131,11 +131,15 @@ isporuka u 1920x1080, audio EBU R 128 standard, trajanje 5 minuta, PAL',
 potpisne liste te osigurati fotografski dokaz mjesečnih koordinacijskih sastanaka','2021-2-20 09:00:00',
 '2021-2-20 12:10:00',8,1,'2021-2-21 09:00:00',2);
 
-insert into members (name, lastName, userName, password, isTeamLeader)
+insert into members (firstName, lastName, userName, password, isTeamLeader)
 values ('Chuck','Norris','N0rr1s','Sifra12345678889',1),('Marko','Marković','MM007','JamesBlond069',0),
 ('Petar','Bočkaj','Guc2A','OsvetaKanižlićeve',0);
 
 insert into activitiesConnector(activityID, memberID) 
 values (1,1),(2,1),(3,1),(1,2),(2,3),(4,3),(5,1),(6,2), (7,3), (7,1), (8,2); 
 
-
+select a.projectName, b.activityName, c.contractActivityName 
+from projects a inner join activities b on a.id = b.projectID
+inner join folders c on b.folderID = c.id
+where b.projectID is not null 
+order by 1 desc;
